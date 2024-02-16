d68d666212db5d6ffb92e20733548235cb6bb356
Karlsooon <karakatalmasova2@gmail.com>
2024-02-17 00:17:03 +0600

    repositories are ready

--- src/main/java/com/karakat/spring/RedditClone/repository/VoteRepository.java
+++ src/main/java/com/karakat/spring/RedditClone/repository/VoteRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+//VoteRepo
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
     Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
