a6f12316697fd533a50e9ae201b13d657112f05f
Karlsooon <karakatalmasova2@gmail.com>
2024-02-17 02:19:46 +0600

    subreddit mapper

---
+++ src/main/java/com/karakat/spring/RedditClone/mapper/SubredditMapper.java
@@ -0,0 +1,26 @@
+package com.karakat.spring.RedditClone.mapper;
+
+
+import com.karakat.spring.RedditClone.dto.SubredditDto;
+import com.karakat.spring.RedditClone.model.Post;
+import com.karakat.spring.RedditClone.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) {
+        return numberOfPosts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+}
