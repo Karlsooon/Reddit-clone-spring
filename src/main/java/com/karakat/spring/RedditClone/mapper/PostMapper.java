8f36143e12c78c6bd06645e88cf6e423bf16ac25
Karlsooon <karakatalmasova2@gmail.com>
2024-02-17 02:18:04 +0600

    comment mapper/Post Mapper

---
+++ src/main/java/com/karakat/spring/RedditClone/mapper/PostMapper.java
@@ -0,0 +1,75 @@
+package com.karakat.spring.RedditClone.mapper;
+
+import com.github.marlonlom.utilities.timeago.TimeAgo;
+
+import com.karakat.spring.RedditClone.dto.PostRequest;
+import com.karakat.spring.RedditClone.dto.PostResponse;
+import com.karakat.spring.RedditClone.model.*;
+import com.karakat.spring.RedditClone.repository.CommentRepository;
+import com.karakat.spring.RedditClone.repository.VoteRepository;
+import com.karakat.spring.RedditClone.service.AuthService;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static com.karakat.spring.RedditClone.model.VoteType.DOWNVOTE;
+import static com.karakat.spring.RedditClone.model.VoteType.UPVOTE;
+
+
+@Mapper(componentModel = "spring")
+public abstract class PostMapper {
+
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private VoteRepository voteRepository;
+    @Autowired
+    private AuthService authService;
+
+
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "description", source = "postRequest.description")
+    @Mapping(target = "subreddit", source = "subreddit")
+    @Mapping(target = "voteCount", constant = "0")
+    @Mapping(target = "user", source = "user")
+    public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
+
+    @Mapping(target = "id", source = "postId")
+    @Mapping(target = "subredditName", source = "subreddit.name")
+    @Mapping(target = "userName", source = "user.username")
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    @Mapping(target = "upVote", expression = "java(isPostUpVoted(post))")
+    @Mapping(target = "downVote", expression = "java(isPostDownVoted(post))")
+    public abstract PostResponse mapToDto(Post post);
+
+    Integer commentCount(Post post) {
+        return commentRepository.findByPost(post).size();
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
+
+    boolean isPostUpVoted(Post post) {
+        return checkVoteType(post, UPVOTE);
+    }
+
+    boolean isPostDownVoted(Post post) {
+        return checkVoteType(post, DOWNVOTE);
+    }
+
+    private boolean checkVoteType(Post post, VoteType voteType) {
+        if (authService.isLoggedIn()) {
+            Optional<Vote> voteForPostByUser =
+                    voteRepository.findTopByPostAndUserOrderByVoteIdDesc(post,
+                            authService.getCurrentUser());
+            return voteForPostByUser.filter(vote -> vote.getVoteType().equals(voteType))
+                    .isPresent();
+        }
+        return false;
+    }
+
+}
