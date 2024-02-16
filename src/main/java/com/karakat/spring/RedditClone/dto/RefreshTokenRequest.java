232de32df8e9cb3202c3b7c64be8ea09be7edb90
Karlsooon <karakatalmasova2@gmail.com>
2024-02-17 01:33:38 +0600

    added full service

---
+++ src/main/java/com/karakat/spring/RedditClone/dto/RefreshTokenRequest.java
@@ -0,0 +1,16 @@
+package com.karakat.spring.RedditClone.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequest {
+    @NotBlank
+    private String refreshToken;
+    private String username;
+}
---
+++ src/main/java/com/karakat/spring/RedditClone/dto/VoteDto.java
@@ -0,0 +1,15 @@
+package com.karakat.spring.RedditClone.dto;
+
+
+import com.karakat.spring.RedditClone.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
